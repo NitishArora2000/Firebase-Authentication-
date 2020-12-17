90d80d345edeb99ece40823e3b95320d8f96fcf8
Nitish Arora <61377353+NitishArora2000@users.noreply.github.com>
2020-12-17 22:33:53 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'SigninPage.dart';
+import 'SignupPage.dart';
+import 'Homepage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'FIREBASE LOGIN',
+        theme: ThemeData.dark(),
+        home: Homepage(),
+        routes: <String, WidgetBuilder>{
+          "/SigninPage": (BuildContext context) => SigninPage(),
+          "/SignupPage": (BuildContext context) => SignupPage(),
+          
+        });
+  }
+}
